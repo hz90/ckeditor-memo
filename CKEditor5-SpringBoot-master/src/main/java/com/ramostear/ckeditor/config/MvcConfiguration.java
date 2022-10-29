23ae0bc2eabd1d3fd2a29b9aba9cc9a40045f30f
DESKTOP-S18FNET\admin <kaisyu.chou@gmail.com>
2022-10-29 13:16:49 +0800

    后台API

---
+++ CKEditor5-SpringBoot-master/src/main/java/com/ramostear/ckeditor/config/MvcConfiguration.java
@@ -0,0 +1,56 @@
+package com.ramostear.ckeditor.config;
+
+import com.ramostear.ckeditor.common.Consts;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
+
+/**
+ * @author :       ramostear/树下魅狐
+ * <p>This java file was created by ramostear in 2020/7/7 0007 17:56.
+ * The following is the description information about this file:</p>
+ * <p>description:</p>
+ */
+@Configuration
+public class MvcConfiguration extends WebMvcConfigurationSupport {
+
+    @Bean
+    public CommonsMultipartResolver multipartResolver(){
+        CommonsMultipartResolver resolver = new CommonsMultipartResolver();
+        resolver.setMaxUploadSize(600000000);
+        resolver.setDefaultEncoding("UTF-8");
+        return resolver;
+    }
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/")
+                .addResourceLocations("file:"+ Consts.FILE_STORAGE_ROOT+"/images/")
+        .addResourceLocations("file:"+ Consts.FILE_STORAGE_ROOT+"/file/")
+        .addResourceLocations("file:"+ Consts.FILE_STORAGE_ROOT+"/video/");
+        super.addResourceHandlers(registry);
+    }
+
+    @Bean
+    public FreeMarkerViewResolver freeMarkerViewResolver(){
+        FreeMarkerViewResolver resolver = new FreeMarkerViewResolver();
+        resolver.setCache(false);
+        resolver.setPrefix("");
+        resolver.setSuffix(".html");
+        return resolver;
+    }
+
+    @Bean
+    public FreeMarkerConfigurer freeMarkerConfigurer(){
+        String[] templatePaths = {"classpath:/templates/","file:"+Consts.FILE_STORAGE_ROOT+"/themes/"};
+        FreeMarkerConfigurer configurer = new FreeMarkerConfigurer();
+        configurer.setTemplateLoaderPaths(templatePaths);
+        return configurer;
+    }
+
+}
---
+++ CKEditor5-SpringBoot-master/src/main/resources/static/js/translations/ca.js
@@ -0,0 +1,1 @@
+(function(d){	const l = d['ca'] = d['ca'] || {};	l.dictionary=Object.assign(		l.dictionary||{},		{"%0 of %1":"",Cancel:"Cancel·lar",Italic:"Cursiva",Save:"Desar","Show more items":""}	);l.getPluralForm=function(n){return (n != 1);;};})(window.CKEDITOR_TRANSLATIONS||(window.CKEDITOR_TRANSLATIONS={}));
